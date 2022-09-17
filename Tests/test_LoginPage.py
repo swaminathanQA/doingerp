7b2fc17128dd442679ca2141918501162a0336f8
swaminathan <easwaminath@gmail.com>
2022-09-17 08:09:34 +0530

    initial draft

--- Tests/test_LoginPage.py
+++ Tests/test_LoginPage.py
@@ -1,4 +1,5 @@
 import time
+import allure
 
 from Config.config import TestData
 from Pages.LoginPage import LoginPage
@@ -6,12 +7,12 @@ from Tests.test_base import BaseTest
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 class Test_Home(BaseTest):
-
+    @allure.description("login to the doing erp portal")
     def test_login(self):
         self.LoginPage = LoginPage(self.driver)
         self.LoginPage.do_login(TestData.USER_NAME, TestData.PASSWORD)
         WebDriver.maximize_window(self.driver)
-        time.sleep(2)
+        time.sleep(5)
         print("Selected Environment URL : " + TestData.BASE_URL)
         print("Logged in User Name : " + TestData.USER_NAME)
 
